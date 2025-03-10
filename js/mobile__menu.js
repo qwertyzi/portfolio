21407195757bd8d3cbebbfdd3cec9b294b6aca6e
Artem <agaleev583@gmail.com>
2025-03-10 16:49:23 +0300

    feat:rewrote the mobile menu function using event delegation

--- js/mobile__menu.js
+++ js/mobile__menu.js
@@ -1,10 +1,40 @@
-const burger = document.querySelector("#hamburger");
+// const burger = document.querySelector("#hamburger");
+// const menu = document.querySelector("#menu")
 
-const mobileMenu = () => {
-    burger.classList.toggle("open");
-}
 
-burger.addEventListener("click", mobileMenu);
+// const mobileMenu = () => {
+//     burger.classList.toggle("open");
+//     menu.classList.toggle("open")
+// }
+
+// burger.addEventListener("click", mobileMenu);
+
 
 // 1.допис стили и скипты
 // 2.превести на делиг событий
+
+const header = document.querySelector("#header");
+
+const mobileMenu = (event) => {
+    console.log(event);
+    
+    const isBurger = event.target.closest("#hamburger")
+
+    if (isBurger) {
+        isBurger.classList.toggle("open");
+        const menu = header.querySelector("#menu")
+        menu.classList.toggle("open")
+    }
+    
+}
+
+header.addEventListener("click", (event) => mobileMenu(event));
+
+
+
+
+
+
+
+
+
