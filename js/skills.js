2ffe766f9bba9005fa65f132b8f5312b75038e9b
Artem <agaleev583@gmail.com>
2025-04-11 17:51:05 +0300

    feat: add first svg icon

--- js/skills.js
+++ js/skills.js
@@ -27,12 +27,30 @@ const cardList = [
 ]
 
 const createListCard = () => {
+    const content = document.querySelector("#skillsContent");
     const listElement = document.createElement("ul");
 
     cardList.forEach(element => {
-        console.log(element);
-        
+        const listItem = document.createElement("li");
+        const card = document.createElement("div");
+        const icon = document.createElement("div");
+        const sprite = 
+        `
+        <svg class="social-icon" viewBox="0 0 24 24" width="114" height="129">
+            <use href="/img/sprite.svg#${element.idSprite}"></use>
+        </svg>
+        `;
+        const title = document.createElement("h6");
+        title.innerText = element.tittle;
+
+        listItem.append(card);
+        card.append(icon);
+        icon.innerHTML = sprite;
+        card.append(title);
+
+        listElement.append(listItem);
     });
+    content.append(listElement);
 }
 
 createListCard()
